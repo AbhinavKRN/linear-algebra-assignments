c967d0fa451365aea5e47aa0dba85cca43b1f988
AbhinavKRNarayan <115357208+AbhinavKRN@users.noreply.github.com>
2024-02-09 12:27:01 +0530

    Update equations.py

--- solutions/equations.py
+++ solutions/equations.py
@@ -5,11 +5,40 @@ def solve(A, b):
     returns: x, where x is the solution to the equation Ax = b
     if no solution exists, return -1
     if infinite solutions exist, return -2"""
-    x = [0] * len(A[0])
-    return x
+    n = len(A)
+        for i in range(n):
+            if A[i][i] == 0:
+                return -1
+            for j in range(n):
+                if i != j:
+                    ans = A[j][i]/A[i][i]
+                    for k in range(n):
+                        A[j][k] = A[j][k] - ans * A[i][k]
+                    B[j] = B[j] - ans * B[i]
+        for i in range(n):
+            if A[i][i] == 0:
+                return -1
+            B[i] = B[i]/A[i][i]
+            A[i][i] = 1
+        return B
+    return gauss(A, B)
 
 
 def det(A):
     """calculates the determinant of A
     if A is not a square matrix, return 0"""
-    return 0
+    if len(A) != len(A[0]):
+        return 0
+    if len(A) == 1:
+        return A[0][0]
+    if len(A) == 2:
+        return A[0][0]*A[1][1] - A[0][1]*A[1][0]
+    else:
+        det = 0
+        for i in range(len(A)):
+            det += ((-1)**i)*A[0][i]*det(minor(A, 0, i))
+        return det
+        
+def minor(i, j, k):
+    return [row[:k] + row[k+1:] for row in (i[:k]+i[j+1:])]
+    
